d1f63e582e2d92c56039ea0a925e66cd97a90d0e
Renato Moreno <renats.mc@gmail.com>
2022-09-11 01:05:07 -0300

    ajusta css para lista não estourar tamanho da tela caso tenha muitos registros

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -19,42 +19,42 @@ export default function List() {
   };
 
   return (
-    <div className="list-container">
-      <h2>Lista de tratamentos</h2>
-
-      {!list.length ? (
-        <p className="list-message">Ainda não tem tratamentos registrados</p>
-      ) : (
-        <table className="table-list">
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Tratamento</th>
-              <th>Data</th>
-              <th>Valor</th>
-              <th>Parcelas</th>
-              <th>Editar</th>
-            </tr>
-          </thead>
-          <tbody>
-            {list.map((item, i) => (
-              <tr key={i}>
-                <td>{item.name}</td>
-                <td>{item.treatment}</td>
-                <td>{formatDate(item.date)}</td>
-                <td>{`R$ ${item.value}`}</td>
-                <td>{`${item.portion} parcela(s)`}</td>
-                <td className="edit-button">
-                  <Link to={`/${item.id}`}>
-                    <BiEdit />
-                  </Link>
-                </td>
+    <div>
+      <div className="list-container">
+        <h2>Lista de tratamentos</h2>
+        {!list.length ? (
+          <p className="list-message">Ainda não tem tratamentos registrados</p>
+        ) : (
+          <table className="table-list">
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Tratamento</th>
+                <th>Data</th>
+                <th>Valor</th>
+                <th>Parcelas</th>
+                <th>Editar</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-
+            </thead>
+            <tbody>
+              {list.map((item, i) => (
+                <tr key={i}>
+                  <td>{item.name}</td>
+                  <td>{item.treatment}</td>
+                  <td>{formatDate(item.date)}</td>
+                  <td>{`R$ ${item.value}`}</td>
+                  <td>{`${item.portion} parcela(s)`}</td>
+                  <td className="edit-button">
+                    <Link to={`/${item.id}`}>
+                      <BiEdit />
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
       {list.length > 0 && (
         <div className="values-container">
           <p>
